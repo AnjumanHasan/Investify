e19eed574de5fa628063a374a185fe3271703f15
Anjuman Hasan <anju2084@gmail.com>
2022-03-12 00:36:32 +0530

    adding initial screens

---
+++ app/src/main/java/thoseITgirls/investify/loginAs.kt
@@ -0,0 +1,11 @@
+package thoseITgirls.investify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginAs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_as)
+    }
+}
