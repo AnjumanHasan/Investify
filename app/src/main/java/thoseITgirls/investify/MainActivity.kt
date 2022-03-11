3cfd628d2aaeb0d8f4a5915dfe7951b0661d84da
Anjuman Hasan <anju2084@gmail.com>
2022-03-11 23:11:46 +0530

    first commit

---
+++ app/src/main/java/thoseITgirls/investify/MainActivity.kt
@@ -0,0 +1,11 @@
+package thoseITgirls.investify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
