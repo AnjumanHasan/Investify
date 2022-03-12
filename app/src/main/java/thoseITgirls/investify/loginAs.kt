49ba230b888fca23223d2bf2a84ad92c07194c96
Anjuman Hasan <anju2084@gmail.com>
2022-03-13 02:07:19 +0530

    added signup screen

--- app/src/main/java/thoseITgirls/investify/loginAs.kt
+++ app/src/main/java/thoseITgirls/investify/loginAs.kt
@@ -1,11 +1,24 @@
 package thoseITgirls.investify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class loginAs : AppCompatActivity() {
+
+    lateinit var businessButton: Button
+    lateinit var investorButton: Button
+    lateinit var loginText : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_as)
+
+        businessButton.setOnClickListener{
+            val intent = Intent(this,businessSignup::class.java)
+            startActivity(intent)
+        }
+
     }
 }
